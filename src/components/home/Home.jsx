6f5bb1dab5490553388eaf8d357f53688cf498a9
Kyle H <kyle@Kyles-MBP.lan>
2021-07-10 23:16:16 -0400

    finished home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,22 @@
 import "./home.scss"
+import { init } from 'ityped';
+import { useEffect, useRef } from 'react';
 
 export default function Home() {
+
+    const textRef = useRef();
+
+    useEffect(()=>{
+        init(textRef.current, {
+            showCursor: true, 
+            backDelay: 1500,
+            backSpeed: 70,
+            strings: ['"gaming warlord"', '"god gamer"', '"puzzle guy"', '"dirt diggler"',
+                      '"obvious cheater"', '"blatant walls"'
+                     ] 
+        })
+
+    }, []);
     
     return (
         <div className="homePage" id="home">
@@ -11,7 +27,10 @@ export default function Home() {
             </div>
             <div className="right">
                 <div className="wrapper">
-                    
+                    <h3>creo</h3>
+                    <h3><span ref={textRef}></span></h3>
+                    <h3 id="after">legend</h3>
+                        
                 </div>
             </div>
         </div>
